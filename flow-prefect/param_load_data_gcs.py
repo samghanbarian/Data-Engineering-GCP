c8906c3a3cdabafa1c3bdfd58e397b1abe96fc6c
samghanbarian <samighanbarian@gmail.com>
2023-05-14 17:38:52 +0200

    fix the problem with month

--- flow-prefect/param_load_data_gcs.py
+++ flow-prefect/param_load_data_gcs.py
@@ -14,41 +14,39 @@ from prefect_gcp.cloud_storage import GcsBucket
 
 
 @task(retries = 3,log_prints=True, tags=["fetch"], cache_key_fn=task_input_hash, cache_expiration=timedelta(days=1))
-def fetch(url:str) -> pd.DataFrame:
+def fetch(url:str,path):
     """ download the data and turn into dataframe"""
-    pq_name = "output.parquet"
-    os.system(f"wget {url} -O {pq_name}")
-    trips = pq.read_table(pq_name)
-    df = trips.to_pandas() #convert to dataframe
-    return df
+    os.system(f"wget {url} -O {path}")
+    return 
 
-@task()
-def write_local_df(df:pd.DataFrame, color:str, dataset_file:str) -> Path:
-    """write data frame locally as a parquet file"""
-    path = Path(f"data/{color}/{dataset_file}.parquet")
-    df.to_parquet(path, compression = 'gzip')
-    return path
 
-@task(retries=3, log_prints=True)
-def write_to_gcs(path:Path) -> None:
-    gcs_block = GcsBucket.load("gcs-ny-taxi")
-    gcs_block.upload_from_path(path)
+
+# @task(retries=3, log_prints=True)
+# def write_to_gcs(path:Path) -> None:
+#     gcs_block = GcsBucket.load("gcs-ny-taxi")
+#     gcs_block.upload_from_path(path)
 
 
 
 @flow(name="load_data")
 def load_data_gcs(color,year,month):
     """ This is the main ETL function"""
-    dataset_file =f"{color}_tripdata_{year}-{month:02}"
-    dataset_url =f"https://d37ci6vzurychx.cloudfront.net/trip-data/{color}_tripdata_{year}-{month:02}.parquet"
-    df = fetch(dataset_url)
-    path = write_local_df(df,color,dataset_file)
-    write_to_gcs(path)
-@flow()
-def parent_flow(color,year,month) -> None:
-    """ parent flow to set the params"""
     for month in month:
+        dataset_file =f"{color}_tripdata_{year}-{month:02}"
+        dataset_url =f"https://d37ci6vzurychx.cloudfront.net/trip-data/{color}_tripdata_{year}-{month:02}.parquet"
+        path = Path(f"data/{color}/{year}/{dataset_file}.parquet")
+        fetch(dataset_url,path)
+        # path = write_local_df(df,color,dataset_file)
+        # write_to_gcs(path)
+@flow()
+def parent_flow_yg(color,year,month) -> None:
+    """ parent flow to set the params"""
+    for year in year:
         load_data_gcs(color,year,month)
 
 if __name__ == "__main__":
-    parent_flow(color,year,month)
+    color = ['yellow','green']
+    year = [2022,2021]
+    month = [1,2,3,4,5,6,7,8,9,10,11,12]
+    for color in color:
+        parent_flow_yg(color,year,month)
