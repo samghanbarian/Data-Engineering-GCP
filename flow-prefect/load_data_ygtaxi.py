3e6d1bfaf14871939783ff64243d86a091bb85f9
sam <samighanbarian@gmail.com>
2023-06-03 11:44:31 +0000

    add spark code for running sql command in spark

--- flow-prefect/load_data_ygtaxi.py
+++ flow-prefect/load_data_ygtaxi.py
@@ -13,10 +13,10 @@ from prefect_gcp.cloud_storage import GcsBucket
 
 
 
-@task(retries = 3,log_prints=True, tags=["fetch"], cache_key_fn=task_input_hash, cache_expiration=timedelta(days=1))
-def fetch(url:str,path):
+@task()
+def fetch(url,path):
     """ download the data and turn into dataframe"""
-    os.system(f"wget {url} -O {path}")
+    os.system(f"wget {url} -O{path}")
     return 
 
 
@@ -31,23 +31,22 @@ def fetch(url:str,path):
 @flow(name="load_data")
 def load_data_gcs(color,year,month):
     """ This is the main ETL function"""
-    dataset_file =f"{color}_tripdata_{year}-{month:02}"
-    dataset_url =f"https://d37ci6vzurychx.cloudfront.net/trip-data/{color}_tripdata_{year}-{month:02}.parquet"
-    path = Path(f"data/{color}/{year}/{dataset_file}.parquet")
-    fetch(dataset_url,path)
-    # path = write_local_df(df,color,dataset_file)
-    # write_to_gcs(path)
+    for month in month:
+        dataset_file =f"{color}_tripdata_{year}-{month:02}"
+        dataset_url =f"https://d37ci6vzurychx.cloudfront.net/trip-data/{color}_tripdata_{year}-{month:02}.parquet"
+        path = Path(f"data/{color}/{year}/{dataset_file}.parquet")
+        fetch(dataset_url,path)
+        # path = write_local_df(df,color,dataset_file)
+        # write_to_gcs(path)
 @flow()
 def parent_flow_yg(color,year,month) -> None:
     """ parent flow to set the params"""
-
-    load_data_gcs(color,year,month)
+    for year in year:
+        load_data_gcs(color,year,month)
 
 if __name__ == "__main__":
     color = ['yellow','green']
     year = [2022,2021]
-    month = [1,2,3,4,5,6]
+    month = [1,2,3,4,5,6,7,8,9,10,11,12]
     for color in color:
-        for year in year:
-            for month in month:
-                parent_flow_yg(color,year,month)
+        parent_flow_yg(color,year,month)
